a6f4fe99ada13f5115aa13b5b5586aecc6727b18
rudyluis <68257300+rudyluis@users.noreply.github.com>
2026-09-05 11:42:47 -0400

    actulizar version para produccion

--- app.py
+++ app.py
@@ -606,4 +606,6 @@ def administracion():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ##app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
