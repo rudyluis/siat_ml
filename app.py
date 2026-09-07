adac40c7c0cb7224036c662adb0858eae12c18c6
rudyluis <68257300+rudyluis@users.noreply.github.com>
2026-09-06 22:12:10 -0400

    hkjhkj

--- app.py
+++ app.py
@@ -901,6 +901,6 @@ def actualizar_configuracion():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    ##port = int(os.environ.get("PORT", 10000))
-    #3app.run(host="0.0.0.0", port=port)
+    ##app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
